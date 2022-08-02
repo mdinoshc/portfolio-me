7f4a933e8d19a083cbdb219cbfbaf100ee016057
mdinoshc <mdinoshc@gmail.com>
2022-08-02 06:58:26 +0530

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function  myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+}
+
+function  myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
+
+function  myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+}
